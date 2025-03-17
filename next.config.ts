bf27cecd747fcc5e3e87c82bebd6e0218e9a9268
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-17 22:04:57 +0100

    add support for dynamic icons and enhance WagmiConnector with login prompt

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { pathname: "/logos/**", protocol: "https", hostname: "cryptologos.cc" },
+      {
+        pathname: "/icons/**",
+        protocol: "https",
+        hostname: "iconic.dynamic-static-assets.com",
+      },
     ],
   },
   experimental: {
--- src/components/providers/wagmi-connector.tsx
+++ src/components/providers/wagmi-connector.tsx
@@ -1,21 +1,57 @@
-"use client"
+"use client";
 
 import React from "react";
-import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import {
+  DynamicEmbeddedWidget,
+  useIsLoggedIn,
+} from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { WagmiProvider } from "wagmi";
 import { config } from "./wagmi";
+import Image from "next/image";
 
 export default function WagmiConnector({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isLogged = useIsLoggedIn();
   return (
     <WagmiProvider config={config}>
       <DynamicWagmiConnector>
-        <DynamicWidget />
-        {children}
+        <div className="relative">
+          {!isLogged && (
+            <div className="fixed z-40 w-screen h-screen bg-secondary/70 backdrop-blur-md flex items-center justify-center">
+              {/* background can be none, default or with-border */}
+              <div className="lg:w-1/3 bg-secondary/90 border border-white/50 rounded-2xl p-8 gap-6 flex flex-col items-center">
+                <Image
+                  src={"/stable-fun-logo.svg"}
+                  alt="Logo"
+                  width={140}
+                  height={34}
+                  priority
+                  className="w-[200px]"
+                />
+
+                <div className="flex flex-col gap-2 items-center mb-4">
+                  <div className="text-xl">
+                    Welcome to{" "}
+                    <span className="text-primary font-medium">Stable.fun</span>
+                  </div>
+                  <p className="text-xs font-light">
+                    You are a step away from using our platform
+                  </p>
+                </div>
+                <DynamicEmbeddedWidget
+                  background="none"
+                  className="text-white"
+                  style={{ color: "white" }}
+                />
+              </div>
+            </div>
+          )}
+          {children}
+        </div>
       </DynamicWagmiConnector>
     </WagmiProvider>
   );
