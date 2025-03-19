cfc59edf6b4eed53dd42d3455b7429edb9153351
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-19 12:54:25 +0100

    add loading state to WagmiConnector; display loading component while checking login status

--- src/components/providers/wagmi-connector.tsx
+++ src/components/providers/wagmi-connector.tsx
@@ -9,19 +9,25 @@ import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { WagmiProvider } from "wagmi";
 import { config } from "./wagmi";
 import Image from "next/image";
+import Loading from "@/app/loading";
 
 export default function WagmiConnector({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isLoaded, setLoaded] = React.useState<boolean>(false);
   const isLogged = useIsLoggedIn();
+
+  React.useEffect(() => {
+    setLoaded(true);
+  }, []);
 
   return (
     <WagmiProvider config={config}>
       <DynamicWagmiConnector>
         <div className="relative">
-          {!isLogged && (
+          {!isLogged && isLoaded && (
             <div className="fixed z-40 w-screen h-screen bg-secondary/70 backdrop-blur-md flex items-center justify-center">
               {/* background can be none, default or with-border */}
               <div className="lg:w-1/3 bg-secondary/90 border border-white/50 rounded-2xl p-8 gap-6 flex flex-col items-center">
@@ -51,6 +57,7 @@ export default function WagmiConnector({
               </div>
             </div>
           )}
+          {!isLoaded && <Loading />}
           {children}
         </div>
       </DynamicWagmiConnector>
