7736f9447349d9256764a68c9f310164ea859548
donjne <davidjrn247@gmail.com>
2025-05-10 15:47:45 +0100

    fix build errors - use client

--- src/app/redeem/page.tsx
+++ src/app/redeem/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { stablecoins, token } from "@/static-data/token";
 import SwapProvider from "./components/swap-context";
 import SwapWindow from "./components/swap-window";
@@ -28,32 +30,4 @@ export default async function SwapPage() {
     </SwapProvider>
   );
 }
-// import RedeemWindow from "./components/redeem-window";
-// import { stablecoins, token } from "@/static-data/token";
-// import RedeemProvider from "./components/redeem-context";
-// import RedeemMiniWindow from "./components/redeem-mini-window";
-// import { TradeWindowToken } from "@/types";
 
-// async function getData(): Promise<TradeWindowToken[]> {
-//   // Fetch data from your API here.
-//   return token as TradeWindowToken[];
-// }
-
-// export default async function RedeemPage() {
-//   const token = await getData();
-
-//   return (
-//     <RedeemProvider>
-//       <div className="space-y-2">
-//         <div className="text-xs text-white/50 mb-3">
-//           Swap your <span className="text-primary">coins</span> instantly
-//         </div>
-
-//         <div className="flex flex-col lg:flex-row gap-4">
-//           <RedeemWindow stablecoins={stablecoins} token={token} />
-//           <RedeemMiniWindow />
-//         </div>
-//       </div>
-//     </RedeemProvider>
-//   );
-// }
